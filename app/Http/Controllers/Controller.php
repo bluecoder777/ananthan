b83afff8d7fb59446eea14dbc83ed42f10d00027
abinopoulose <65918091+abinopoulose@users.noreply.github.com>
2022-07-20 17:17:43 +0530

    first commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    function index() 
+    {
+        $a="";
+        exec('python C:\Users\abino_c2admvs\Desktop\ServerJefrin\mlscript.py',$a);
+        dd($a);
+        return view('welcome');
+    }
+    function abcd(Request $request) 
+    {
+        $b=$request->qwerty;
+        move_uploaded_file($b, "C:\Users\abino_c2admvs\Desktop\ServerJefrin\qw.jpg");
+        exec('python C:\Users\abino_c2admvs\Desktop\ServerJefrin\mlscript.py',$a);
+        $c=json_encode($a);
+        return $b;
+    }
+    
+    
+}
